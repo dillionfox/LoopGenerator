3032ee9a313e665d235ded69135203c006b2a5f6
dillionfox <11foxd1@gmail.com>
2018-03-16 16:43:34 -0400

    added comments

--- LoopGenerator.py
+++ LoopGenerator.py
@@ -1,40 +1,70 @@
 #!/usr/bin/env python
 """
-Cyclic Coordinate Descent (CCD)
-Parts of this class were borrowed from https://github.com/andre-wojtowicz/ccd-loop-closer/blob/master/CCDLoopCloser.py 
-Based on article 'Cyclic coordinate descent: A robotics algorithm for protein loop closure.' - Canutescu AA, Dunbrack RL Jr.
+REQUIREMENTS: Numpy, BioPython, CCD, and LoopTools.
+This code was designed to take a protein structure, make a copy of it,
+shift it, and then connect the N-terminus of the original protein
+to the C-terminus of the copied protein with a LOOP. The loop
+can be generated 2 different ways, but the first method (denoted
+OPTION 1) is the highly preferred method. There are several other ways
+of doing this, but OPTION 1 works well enough that I didn't see a
+need to pursue any of them.
+
+Dillion Fox, 3/2018
+
 """
+
 import numpy as np; from numpy import linalg as npl
 from numpy.random import random
 import Bio.PDB
 import CCD
 import LoopTools
 
-print "add scoring function to assess quality of loops!"
+print "~~~ TODO ~~~"
+print "1. I think the next thing to do is figure out how to port this"
+print "code into AUTOMACS. From there I can figure out how to write"
+print "pdb files containing the combined structures, and I can also"
+print "have it throw away structures that have steric clashes."
+print ""
+print "2. make it work for coarse-grained models"
+print ""
 
-############################################
-### load variables into global namespace ###
-############################################
-global RMSD_threshold; RMSD_threshold = 0.2
-global max_iterations; max_iterations = 300
-global max_success; max_success = 10 # maximum number of structures you want to make
-global dl; dl = 0 # variability in chain length. i.e. 15 +/- 1
-global DIST_FACTOR; DIST_FACTOR = 0.25
+#########################################################
+### load USER DEFINED variables into global namespace ###
+#########################################################
 
-########################################################
-### Graft existing loops from the pdb onto structure ###
-########################################################
+global RMSD_threshold; RMSD_threshold = 0.15			# cut off for loop closing
+global max_iterations; max_iterations = 10000			# number of iterations for CCD algorithm
+global max_success;    max_success = 1 				# maximum number of structures you want to make
+global dl;             dl = 0 					# variability in chain length. i.e. 15 +/- 1
+global DIST_FACTOR;    DIST_FACTOR = 0.25			# N-to-C terminal distance variability for searching ArchDB for structures
+								# pdbname is your input structure
+global pdbname ;       pdbname = "/home/dillion/data/reflectin/add_linker/structures/small_mem_protein-inserted_frame-738.pdb"
+global protein_shift;  protein_shift  = np.array([30,0,0])	# how much you want to shift your input structure
+
+
 def mine_loop_data(loop_length,CC_DIST,LEN_TOL):
-	OUTFILE = "loop_dihedrals.out"
-	ArchDB = "ArchDB/archdb.mini.tab"
-	DIST_TOL = np.floor(CC_DIST*DIST_FACTOR)
-	numResLoop = range(int(loop_length-LEN_TOL),int(loop_length+LEN_TOL+1))						# desired number of residues in loop
+	"""
+	This function calls the LoopMiner class and extracts dihedral data from
+	ArchDB and writes it to a file.
+
+	"""
+
+	OUTFILE = "loop_dihedrals.out"				# write dihedrals to output file
+	ArchDB = "ArchDB/archdb.mini.tab"			# ArchDB database
+	DIST_TOL = np.floor(CC_DIST*DIST_FACTOR)		# tolerance for identifying geometrically similar loops
+	numResLoop = range(int(loop_length-LEN_TOL),int(loop_length+LEN_TOL+1))	# desired number of residues in loop
 	print "looking for loops with", loop_length, "+/-", LEN_TOL, ", and span a distance of", CC_DIST, "+/-", DIST_TOL
 	miner = LoopTools.LoopMiner(numResLoop,CC_DIST,DIST_TOL,OUTFILE)
-	dihedral_list = miner.mineArchDB(ArchDB)							# mineArchDB is basically main function, it starts everything and ends there too
+	dihedral_list = miner.mineArchDB(ArchDB)
 	return dihedral_list
 
 def extract_coors(structure):
+	"""
+	This function extracts coordinates from a BioPython structure and returns
+	a numpy array
+
+	"""
+
 	import Bio.PDB
 	v = []
 	for model in structure:
@@ -52,6 +82,13 @@ def extract_coors(structure):
 	return v
 
 def compute_CC_DIST(first_res,target_coors):
+	"""
+	This function computes the C-terminal to N-terminal distance that
+	the linker will have to connect. This data is readily accessible in 
+	ArchDB so you can pre-select loops that have similar geometry to yours
+
+	"""
+
 	Bio.PDB.PDBIO().set_structure(first_res)
 	l = []
 	base_coors = []
@@ -68,7 +105,12 @@ def compute_CC_DIST(first_res,target_coors):
 	return npl.norm(CC_DIST)
 
 def run_CCD(chain,target_coors,success=0,n=1):
-	### RUN CCD ###
+	"""
+	This is the main function that calls the Cyclic Coordinate Descent 
+	class functions
+
+	"""
+
 	init = CCD.CCD(chain, target_coors) 
 	check, RMSD, it, V = init.run(n,max_it=max_iterations,threshold=RMSD_threshold)
 	if check == 'success': 
@@ -77,14 +119,34 @@ def run_CCD(chain,target_coors,success=0,n=1):
 	return [success, V]
 
 def make_first_res():
-	pdbname = "/home/dillion/data/reflectin/add_linker/structures/small_mem_protein-inserted_frame-738.pdb"
-	protein_shift = np.array([30,0,0])
+	"""
+	This function loads your structure with BioPython and extracts the 
+	LAST residue. This residue will be the FIRST residue of the loop, but
+	will later be deleted. I made it this way to ensure a smooth transition
+	from native structure to loop. It was the easiest way that I could think 
+	of to check the dihedrals of all of the loop residues.
+
+	"""
+
 	loopmaker = LoopTools.LoopMaker()
 	[base_res, target_coors] = loopmaker.define_initial_cond(pdbname,protein_shift)
 	first_res = loopmaker.anchor_is_start(base_res)
 	return [first_res,target_coors]
 
+################
+### OPTION 1 ###
+################
 def run_graft(loop_length):
+	"""
+	This function mines a Loop Database to find naturally occurring loop
+	structures with criteria that matches the loop you want to generate.
+	It extracts the dihedrals from the backbone of the loop from the 
+	database and builds a new backbone. It will then close the loop
+	using the Cyclic Coordinate Descent algorithm. Each closed loop
+	it generates is assigned an energetic score based on the CHARMM
+	method of computing the Dihedral Angle Potential (V_DA)
+
+	"""
 	[first_res,target_coors] = make_first_res()
 	CC_DIST = compute_CC_DIST(first_res,target_coors)
 	dihedral_list = [] ; LEN_TOL = 0
@@ -109,10 +171,17 @@ def run_graft(loop_length):
 			exit()
 	return None
 
-###############################################################################################
-### this function starts the chain with the appropriate dihedrals, direction, and placement ###
-###############################################################################################
+################
+### OPTION 2 ###
+################
 def run_straight_aligned(loop_length):
+	"""
+	This is a much simpler, though significantly less effective, option 
+	for generating loops. It builds a straight chain and uses CCD to try
+	to close it. If CCD can't close it, you're screwed. 
+
+	"""
+
 	print "WARNING: this method usually doesn't give good results"
 	[first_res,target_coors] = make_first_res()
 	loopmaker = LoopTools.LoopMaker()
